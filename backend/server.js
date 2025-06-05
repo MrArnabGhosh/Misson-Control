8717a09904854d82d2e5509beb9ed38ff8915574
MrArnabGhosh <ghosharnab460@gmail.com>
2025-06-05 15:17:46 +0530

    Add extra file and connect database

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express"
+import cors from "cors"
+import path from "path"
+import connectDB from './config/db.js'
+
+const app=express();
+
+app.use(
+    cors({
+        origin:process.env.CLIENT_URL || "*",
+        methods:["GET","POST","PUT","DELETE"],
+        allowedHeaders:["Content-Type","Authorization"]
+    })
+)
+
+connectDB()
+
+// middlewares
+app.use(express.json())
+
+
+// routes
+// app.use("/api/auth",authRoutes)
+// app.use("/api/users",userRoutes)
+// app.use("/api/tasks",taskRoutes)
+// app.use("/api/reports",reportRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`))
+
--- frontend/missoncontrol/src/routes/PrivateRoute.jsx
+++ frontend/missoncontrol/src/routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const PrivateRoute = ({allowedRoles}) => {
-  return <Outlet>{allowedRoles}</Outlet>
+  return <Outlet />
 }
 
 export default PrivateRoute
